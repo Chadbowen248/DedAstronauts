1aa453d0b3c8639c16143fd31098a2dcb3156007
Chad Bowen <chadbowen248@gmail.com>
2022-02-12 22:21:55 -0600

    wow think mostly done

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,22 +1,55 @@
-exports.handler = async function () {
-  const { XummSdk } = require('xumm-sdk')
-  const Sdk = new XummSdk(process.env.XUMM_APIKEY, process.env.XUMM_APISECRET)
+exports.handler = async function (event, context, callback) {
+  const { XummSdk } = require("xumm-sdk");
+  const Sdk = new XummSdk(process.env.XUMM_APIKEY, process.env.XUMM_APISECRET);
+  const xrpl = require("xrpl");
+  const client = new xrpl.Client("wss://xls20-sandbox.rippletest.net:51233");
 
+  await client.connect();
+  const nfts = await client.request({
+    method: "account_nfts",
+    account: "rPuPn48C2Z3Db5j14vzHdyq6c32UKXJAtP",
+  });
+
+  // maybe make this random
+
+  const nftTokenIDToMint = nfts.result.account_nfts[0].TokenID;
+
+  let nftSellOffers;
+  try {
+    nftSellOffers = await client.request({
+      method: "nft_sell_offers",
+      tokenid: nftTokenIDToMint,
+    });
+  } catch (err) {
+    console.log("No sell offers.");
+  }
+
+  const tokenIndex = nftSellOffers.result.offers[0].index;
+
+  client.disconnect();
+  const formatURI = URI => {
+    const original = xrpl.convertHexToString(URI).substring(7)
+    return `https://gateway.pinata.cloud/ipfs/${original}`
+  }
   const request = {
-    "TransactionType": "NFTokenAcceptOffer",
-    "Account": "r9gx2kJVyN6AKRSDbro5thyvVLUzUBYZ6p",
-    "SellOffer": "A5CF3E7DE7A6A0E27C1F5F590CF82D790A1ACB08482889679A2EF5022CA54C95" // this comes from the index value from the nft array offer on the ded account
-}
+    TransactionType: "NFTokenAcceptOffer",
+    Account: "rPuPn48C2Z3Db5j14vzHdyq6c32UKXJAtP",
+    SellOffer: tokenIndex, // this comes from the index value from the nft array offer on the ded account
+  };
 
-  const payload = await Sdk.payload.create(request, true)
+  console.log(request);
 
-
+  const payload = await Sdk.payload.create(request, true);
+  const formatedPayload = {
+    ...payload,
+    freshMint: {
+      ...nfts.result.account_nfts[0],
+      formattedURI: formatURI(nfts.result.account_nfts[0].URI)
+    }
+  }
 
   return {
     statusCode: 200,
-    body: JSON.stringify(payload)
-  }
-}
-
-
-
+    body: JSON.stringify(formatedPayload),
+  };
+};
--- netlify/functions/validate.js
+++ netlify/functions/validate.js
@@ -1,33 +1,55 @@
 exports.handler = async function (event, context, callback) {
-  const { TxData } = require("xrpl-txdata");
   const { XummSdk } = require("xumm-sdk");
+  const verifySignature = require('verify-xrpl-signature').verifySignature
+  const xrpl = require("xrpl");
+
+
   const Sdk = new XummSdk(process.env.XUMM_APIKEY, process.env.XUMM_APISECRET);
-  const Verify = new TxData();
+  const client = new xrpl.Client("wss://xls20-sandbox.rippletest.net:51233");
   const result = await Sdk.payload.get(event.queryStringParameters.uuid);
-  const verifiedPayload = await Verify.getOne(result?.response.txid);
-console.log(verifiedPayload)
-  const checkAmount = verifiedPayload.result.Amount === "10000"; 
-  const isValid = verifiedPayload.result.validated === true;
-  const balanceChanged = verifiedPayload.balanceChanges.rKJYFcS28vZW4X17SVzbCeiLN7PaWfSukX[0].value ===
-    "0.01";
 
-    console.log(checkAmount, isValid, balanceChanged)
+  const verified = verifySignature(result.response.hex)
 
-    if(checkAmount && isValid && balanceChanged) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          hash: verifiedPayload.result.hash
-        }),
-      };
+	await client.connect()
+  console.log("Connected to Sandbox")
+	const nfts = await client.request({
+		method: "account_nfts",
+		account: verified.signedBy
+	})
+
+  // <!-- https://gateway.pinata.cloud/ipfs/QmejZCt4xD7tJKAt1qw34PkTDEyM4uvzQa4FjfUvmC9ort/16.png -->
+  // ipfs://QmejZCt4xD7tJKAt1qw34PkTDEyM4uvzQa4FjfUvmC9ort/84.png
+
+  const formatURI = URI => {
+    const original = xrpl.convertHexToString(URI).substring(7)
+    return `https://gateway.pinata.cloud/ipfs/${original}`
+  }
+
+  const userNFTs = nfts.result.account_nfts.map(nft => {
+    return {
+      ...nft,
+      formattedURI: formatURI(nft.URI)
     }
+  })
+	console.log(nfts)
+	client.disconnect()
+
+  console.log(verified)
+
+  console.log(result)
+
 
     return {
-      statusCode: 400,
+      statusCode: 200,
       body: JSON.stringify({
-        error: "something went wrong."
+        verifiedPayload: verified,
+        signerNFTs: userNFTs,
+        hash: result.response.hex
       })
     }
+
+
+// https://gateway.pinata.cloud/ipfs/QmejZCt4xD7tJKAt1qw34PkTDEyM4uvzQa4FjfUvmC9ort/32.png
 
  
 };
